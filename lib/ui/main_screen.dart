f1de2a17159bad0cd4e3134602e4f3ce7e722977
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-03-31 12:02:47 +0900

    feat: implement login screen
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

---
+++ lib/ui/main_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("펫밀리")),
+      body: Column(children: []),
+    );
+  }
+}
