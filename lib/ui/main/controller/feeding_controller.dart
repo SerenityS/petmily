e4bcc2e092294dda884372ef30dacef9c5c3e73c
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-05-27 14:06:23 +0900

    feat: add timer for getDeviceData
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/ui/main/controller/feeding_controller.dart
+++ lib/ui/main/controller/feeding_controller.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:petmily/controller/petmily_controller.dart';
 
 class FeedingPageController extends GetxController {
+  late Timer timer;
+
   Rx<double> panelHeightOpen = 0.0.obs;
   Rx<double> panelHeight = 95.0.obs;
   final double panelHeightClosed = 95.0;
@@ -13,4 +18,18 @@ class FeedingPageController extends GetxController {
     topLeft: Radius.circular(24.0),
     topRight: Radius.circular(24.0),
   );
+
+  @override
+  void onReady() {
+    super.onReady();
+    timer = Timer.periodic(const Duration(seconds: 10), (counter) async {
+      await Get.find<PetmilyController>().getDeviceData();
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
 }
