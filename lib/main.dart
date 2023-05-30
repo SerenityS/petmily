33696a5443812a21861872ea658608129b83bcff
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-05-31 08:05:41 +0900

    fix: resolve permission problem
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:petmily/binding/dependency_binding.dart';
 import 'package:petmily/service/secure_storage_service.dart';
 import 'package:petmily/ui/login/login_screen.dart';
@@ -9,7 +10,15 @@ import 'package:petmily/ui/login/login_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Get.putAsync(() => SecureStorageService().init());
-  initializeDateFormatting().then((_) => runApp(const MyApp()));
+  [
+    Permission.location,
+    Permission.storage,
+    Permission.bluetooth,
+    Permission.bluetoothConnect,
+    Permission.bluetoothScan,
+  ].request().then((status) {
+    initializeDateFormatting().then((_) => runApp(const MyApp()));
+  });
 }
 
 class MyApp extends StatelessWidget {
